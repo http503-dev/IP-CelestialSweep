8b77a6e6a493dd0b909bbedce0189f9fea23c4ea
http503-dev <s10258446@connect.np.edu.sg>
2024-01-29 16:09:04 +0800

    restdb actually working?

---
+++ scripts.js
@@ -0,0 +1,35 @@
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+  
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    // Create an object with the data to be sent to the database
+    const data = {
+      username: username,
+      password: password,
+    };
+  
+    const apiUrl = 'https://ipdatabase-b530.restdb.io/rest/userdatabase';
+    // Make a POST request to update the database
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-apikey': '65b74f425a960f46217795a7',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        // Handle the response data if needed
+        console.log(responseData);
+        
+        // Redirect to the home page after successful login
+        window.location.href = 'index.html';
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
+  
